74825d86aab495c7e94a7c82c45f22afb9da41f1
Pablo Torres <pablo.torres@deri.org>
2013-11-13 17:42:58 +0000

    new version 0.0.4 Fix some bugs

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(
     name='clean_text',
-    version='0.0.3',
+    version='0.0.4',
     description='Text cleaner. Remove stopwords and perform stemming',
     long_description = readme(),
     classifiers=[
